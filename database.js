eaeca48a5694049deae43237ebd20dfcd9587fd6
yuuzukatsu <67664879+yuuzukatsu@users.noreply.github.com>
2022-10-04 11:29:33 +0700

    Update database.js

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ let mysql = require('mysql');
 
 let connection = mysql.createConnection({
     host: 'sql6.freemysqlhosting.net',
-    user: 'sql6516518',
-    password: 'bk1Mtz4dR6',
-    database: 'sql6516518'
+    user: 'sql6524063',
+    password: 'su3KB4XPsf',
+    database: 'sql6524063'
 });
 
 connection.connect(function(error){
